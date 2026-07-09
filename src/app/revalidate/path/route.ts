e497f20d1d9eef3d90b710a5c105daed3b38a816
Valentín Benítez <valentin.benitez@beetransfer.net>
2026-07-09 16:30:24 -0300

    feat: route to revalidate cache by path was added

---
+++ src/app/revalidate/path/route.ts
@@ -0,0 +1,24 @@
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
+
+type Type = 'page' | 'layout';
+
+export function GET(request: NextRequest) {
+    const path = request.nextUrl.searchParams.get("path");
+    const type = request.nextUrl.searchParams.get("type") as Type;
+    const secretParam = request.nextUrl.searchParams.get("secret");
+    const secret = process.env.SECRET;
+    const defaultType = type || 'page';
+
+    if (secretParam !== secret) {
+        return NextResponse.json({ error: "Secret no validated" }, { status: 401 })
+    }
+
+    if (!path) {
+        return NextResponse.json({ error: "Tag is required" }, { status: 400 })
+    }
+
+    revalidatePath(path, defaultType)
+
+    return NextResponse.json({ message: "Cache revalidated" })
+}
